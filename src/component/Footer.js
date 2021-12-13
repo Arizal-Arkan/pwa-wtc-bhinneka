8aa7287b8adc453179f23d03ef8a82b071cfcb30
Danis Zaidan <daniszaidan@gmail.com>
2021-12-13 14:16:00 +0700

    responsive hero

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 function Footer() {
-    return (
-        <div className='justify-between mb-10 flex bg-gray-800 m-auto p-8 items-center rounded-3xl text-white'>
-            <h2 className='text-xl'>WatchLur</h2>
-            <p>© 2021 WatchLur. All rights reserved</p>
-        </div>
-    )
+  return (
+    <div className="justify-between mb-10 flex bg-gray-800 m-auto p-8 items-center rounded-3xl text-white">
+      <h2 className="text-xl">WatchLur</h2>
+      <p>© 2021 WatchLur. All rights reserved</p>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;

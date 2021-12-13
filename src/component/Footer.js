ffcb3d4279ef2fabdc555afdcd366908bcb76b8e
Danis Zaidan <daniszaidan@gmail.com>
2021-12-13 14:23:00 +0700

    responsive footer & menu

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './../style.css';
 
 function Footer() {
   return (
-    <div className="justify-between mb-10 flex bg-gray-800 m-auto p-8 items-center rounded-3xl text-white">
+    <div className="justify-between mb-10 flex bg-gray-800 m-auto p-8 items-center rounded-3xl text-white footer">
       <h2 className="text-xl">WatchLur</h2>
       <p>© 2021 WatchLur. All rights reserved</p>
     </div>
